01d25c9594d697e97d6d979f9b970372951c6eae
Nanae <42784268+nanaenolab@users.noreply.github.com>
2021-10-01 16:37:49 -0400

    unit4

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,10 +17,10 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
-        
-        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
 
     }
 
@@ -100,7 +100,14 @@ class HomeTableViewController: UITableViewController {
             
             cell.profileImageView.image = UIImage(data: imageData)
         }
-        return cell
+        
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        
+        return cell 
     }
     
     
